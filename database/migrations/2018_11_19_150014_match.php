c795e71c57830744771f69344ff7072099b9b80e
Peter Makadi <peter@b2cprint.com>
2018-11-19 21:23:33 +0200

    matches table migration

--- database/migrations/2018_11_19_150014_match.php
+++ database/migrations/2018_11_19_150014_match.php
@@ -13,7 +13,53 @@ class Match extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('matches', function (Blueprint $table) {
+            $table->integer('id');
+            $table->integer('country_id')->unsigned();
+            $table->foreign('country_id')
+                ->references('id')
+                ->on('countries')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('league_id')->unsigned();
+            $table->foreign('league_id')
+                ->references('id')
+                ->on('leagues')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('hometeam')->unsigned();
+            $table->foreign('hometeam')
+                ->references('id')
+                ->on('teams')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('awayteam')->unsigned();
+            $table->foreign('awayteam')
+                ->references('id')
+                ->on('teams')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('date');
+            $table->string('time');
+            $table->string('status');
+            $table->boolean('live');
+            $table->integer('hometeam_score');
+            $table->integer('awayteam_score');
+            $table->integer('hometeam_halftime_score');
+            $table->integer('awayteam_halftime_score');
+            $table->integer('hometeam_extra_score');
+            $table->integer('awayteam_extra_score');
+            $table->integer('hometeam_penalty_score');
+            $table->integer('awayteam_penalty_score');
+            $table->string('hometeam_system');
+            $table->string('awayteam_system');
+            $table->text('goalscorer');
+            $table->text('cards');
+            $table->text('hometeam_lineup');
+            $table->text('awayteam_lineup');
+            $table->text('statistics');
+            $table->timestamps();
+        });
     }
 
     /**
